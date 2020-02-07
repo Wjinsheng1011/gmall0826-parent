5dfb33ed0ed915c74cc2ee52e241b427fdbcf976
WjinSheng <694206260@qq.com>
2020-02-07 08:34:48 +0800

    二次修改

---
+++ gmall0826-logger/src/main/java/com/atguigu/gmall0826/logger/Gmall0826LoggerApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall0826.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0826LoggerApplication {
+
+    //启动的入口
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0826LoggerApplication.class, args);
+    }
+
+}
