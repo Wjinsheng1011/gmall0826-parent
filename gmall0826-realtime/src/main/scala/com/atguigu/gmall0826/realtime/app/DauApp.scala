5732699e754b638b57e2bfec3b0772786c7bd901
WjinSheng <694206260@qq.com>
2020-02-09 14:44:43 +0800

    数据去重，组内过滤

--- gmall0826-realtime/src/main/scala/com/atguigu/gmall0826/realtime/app/DauApp.scala
+++ gmall0826-realtime/src/main/scala/com/atguigu/gmall0826/realtime/app/DauApp.scala
@@ -1,14 +1,18 @@
 package com.atguigu.gmall0826.realtime.app
 
-import java.sql.Date
+
 import java.text.SimpleDateFormat
+import java.util
+import java.util.Date
 
 import com.alibaba.fastjson.JSON
 import com.atguigu.gmall0826.common.GmallConstant
 import com.atguigu.gmall0826.realtime.bean.StartupLog
-import com.atguigu.gmall0826.realtime.util.MyKafkaUtil
+import com.atguigu.gmall0826.realtime.util.{MyKafkaUtil, RedisUtil}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.json4s.DateFormat
@@ -49,17 +53,87 @@ object DauApp {
 
     //  然后再利用清单进行过滤筛选 把清单中已有的用户的新日志过滤掉
 
+    /* 链接redis次数太多可以进一步优化
+    val filteredDstream = startUpLogDstream.filter { startuplog =>
+      val jedis: Jedis = RedisUtil.getJedisClient
+      val dauKey = "dau:" + startuplog.logDate
+      val flag = !jedis.sismember(dauKey, startuplog.mid)
+
+      jedis.close()
+      flag
+    }*/
+    //优化： 利用driver查询出完整清单，然后利用广播变量发送给各个executor
+    //各个ex 利用广播变量中的清单 检查自己的数据是否需要过滤 在清单中的一律清晰掉
+    /*
+    错误方案，会造成driver只执行了一次  不会周期性执行
+    //1 查 driver
+    val jedis: Jedis = RedisUtil.getJedisClient
+    val today: String = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+    val daukey = "dau:"+ today
+    val midSet: util.Set[String] = jedis.smembers(daukey)
+    jedis.close()
+
+    //2.发
+    val midBC: Broadcast[util.Set[String]] = ssc.sparkContext.broadcast(midSet)
+
+    startUpLogDstream.filter{startuplog=> // 3. ex 收 筛查
+      val midSet: util.Set[String] = midBC.value
+      !midSet.contains(startuplog.mid)
+    }
+*/
+    //让driver每5秒执行一次
+    val filteredDstream: DStream[StartupLog] = startUpLogDstream.transform { rdd =>
+      // driver
+      //1 查 driver
+      println("过滤前" + rdd.count())
+      val jedis: Jedis = RedisUtil.getJedisClient
+      val today: String = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+      val daukey = "dau:" + today
+      val midSet: util.Set[String] = jedis.smembers(daukey)
+      jedis.close()
+
+      //2.发
+      val midBC: Broadcast[util.Set[String]] = ssc.sparkContext.broadcast(midSet)
+
+
+      val filteredRDD: RDD[StartupLog] = rdd.filter { startuplog =>
+        // ex 3. ex 收 筛查
+        val midSet: util.Set[String] = midBC.value
+        !midSet.contains(startuplog.mid)
+
+      }
+      println("过滤后" + filteredRDD.count())
+      filteredRDD
+      //driver
+    }
+
+  //  自检内部  分组去重 ： 0 转换成kv  1   先按mid进行分组   2 组内排序 按时间排序  3  取 top1
+    val groupbyMidDstream: DStream[(String, Iterable[StartupLog])] = filteredDstream.map(startuplog=>(startuplog.mid,startuplog)).groupByKey()
+    val realFilteredDstream: DStream[StartupLog] = groupbyMidDstream.flatMap { case (mid, startuplogItr) =>
+      val startuplogList: List[StartupLog] = startuplogItr.toList.sortWith { (startuplog1, startuplog2) =>
+        startuplog1.ts < startuplog2.ts // 正序排序
+      }
+      val top1startupLogList: List[StartupLog] = startuplogList.take(1)
+      top1startupLogList
+    }
+
+
+
+
 
     //  利用redis保存当日访问过的用户清单
 
 
-    startUpLogDstream.foreachRDD { rdd =>
+    realFilteredDstream.foreachRDD { rdd =>
       rdd.foreachPartition { startupLogItr =>
         //写入Rdis
-        val jedis = new Jedis("hadoop102",6379)
+//        val jedis = new Jedis("hadoop102",6379)
+        val jedis: Jedis = RedisUtil.getJedisClient
         for (startupLog <- startupLogItr) {
           //type类型? set(不能重复)  key? dau:2020-02-07  value? mid
           val dauKey = "dau:" + startupLog.logDate
+
+          println(startupLog)
           jedis.sadd(dauKey, startupLog.mid)
           jedis.expire(dauKey, 60 * 60 * 24) //保留24小时,失效
         }
